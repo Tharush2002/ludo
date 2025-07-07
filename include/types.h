58d61ecbe9b6ef2102c5b7a5e34002a8d5169234
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-07-07 23:53:58 +0530

    Recreated the piece struct's logic

--- include/types.h
+++ include/types.h
@@ -3,10 +3,19 @@
 
 #include "raylib.h"
 
+typedef enum {STANDARD,HOME,BASE} SquareType;
+typedef enum {COLOUR_RED,COLOUR_BLUE,COLOUR_GREEN,COLOUR_YELLOW} Colour;
+typedef enum {PIECE_STANDARD,PIECE_HOME,PIECE_BASE,PIECE_FINISHED} PieceStatus;
+
+typedef struct{
+	int index, x, y;
+} Square;
+
 typedef struct {
-    int board_index; // logical square index (0-51)
-    float x, y;      // current pixel position (for animation)
-    Color color;
+	Square *square; 
+	float current_x, current_y;
+	PieceStatus status;
+    	Colour colour;
 } Piece;
 
 #endif
