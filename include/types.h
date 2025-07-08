85c238653cd0de9e0c8e97cf96c4d7e12eec6bde
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-07-08 14:38:30 +0530

    moved the constants to types header

--- include/types.h
+++ include/types.h
@@ -1,7 +1,17 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include "raylib.h"
+//#include "raylib.h"
+
+//#define SQUARE_SIZE 62
+//#define PIECE_SCALE 0.8f
+//#define PIECE_TARGET_SIZE (SQUARE_SIZE * PIECE_SCALE)
+#define NUM_STANDARD_SQUARES 52
+#define NUM_BASE_SQUARES 4
+#define NUM_HOME_SQUARES 5
+#define NUM_PIECES 4
+#define NUM_OPPONENTS 4
+#define MOVE_SPEED 8.0f
 
 typedef enum {STANDARD,HOME,BASE} SquareType;
 typedef enum {COLOUR_RED,COLOUR_BLUE,COLOUR_GREEN,COLOUR_YELLOW} Colour;
