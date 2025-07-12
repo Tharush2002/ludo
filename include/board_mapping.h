43efdd82615467826f88c262c6505bab76550c8e
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-07-12 09:40:43 +0530

    updated to include the approach, home cells

--- include/board_mapping.h
+++ include/board_mapping.h
@@ -6,9 +6,18 @@
 #include <stdio.h>
 #include <string.h>
 
-extern Square standard[52];
-extern Square base[NUM_OPPONENTS][4];
-extern Square home[NUM_OPPONENTS][5];
+#define RED_APPROACH 24
+#define RED_START 26
+#define BLUE_APPROACH 11
+#define BLUE_START 13
+#define GREEN_APPROACH 37
+#define GREEN_START 39
+#define YELLOW_APPROACH 50
+#define YELLOW_START 0
+
+extern Square standard[NUM_STANDARD_SQUARES];
+extern Square base[NUM_OPPONENTS][NUM_BASE_SQUARES];
+extern Square home[NUM_OPPONENTS][NUM_HOME_SQUARES];
  
 int load_board_map(const char *filename);
 void load_squares(FILE *file, SquareType type);
