e0642ca141edb12b24ddb4323abc5d4803e952d4
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-08-01 00:31:21 +0530

    Included the render function to add the dice

--- include/gui.h
+++ include/gui.h
@@ -11,6 +11,7 @@ void init_gui(void);
 void cleanup_gui(void);
 void render_game(Piece (*pieces)[NUM_PIECES]);
 Texture2D get_texture(Colour colour);
+Texture2D get_dice(int value);
 int should_close_window(void);
 
 #endif
