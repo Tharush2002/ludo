9776ea7312d24efef41c756dbfe4ae0003a8b837
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-08-01 00:30:36 +0530

    Rendered the dice to the game

--- src/gui.c
+++ src/gui.c
@@ -1,6 +1,6 @@
 #include "gui.h"
 
-static Texture2D board_texture, red_piece, blue_piece, green_piece, yellow_piece;
+static Texture2D board_texture, red_piece, blue_piece, green_piece, yellow_piece, dice1, dice2, dice3, dice4, dice5, dice6;
 
 void init_gui(void) {
 	InitWindow(1024, 1024, "Ludo Board Game");
@@ -9,6 +9,12 @@ void init_gui(void) {
 	blue_piece = LoadTexture("./assets/blue.png");
 	green_piece = LoadTexture("./assets/green.png");
 	yellow_piece = LoadTexture("./assets/yellow.png");
+	dice1 = LoadTexture("./assets/1.png");
+	dice2 = LoadTexture("./assets/2.png");
+	dice3 = LoadTexture("./assets/3.png");
+	dice4 = LoadTexture("./assets/4.png");
+	dice5 = LoadTexture("./assets/5.png");
+	dice6 = LoadTexture("./assets/6.png");
 	SetTargetFPS(60);
 }
 
@@ -25,6 +31,16 @@ void render_game(Piece (*pieces)[NUM_PIECES]) {
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
 	DrawTexture(board_texture, 0, 0, WHITE);
+
+	if(game.dice!=0){
+		Texture2D dice = get_dice(game.dice);
+		DrawTexture(
+			dice,
+			512 - dice.width / 2,
+			512 - dice.height / 2,
+			WHITE
+		);
+	}
 
 	for (int i = 0; i < NUM_OPPONENTS ; i++) {
 		Texture2D piece_texture = get_texture(pieces[i][0].colour);
@@ -51,6 +67,20 @@ Texture2D get_texture(Colour colour){
 		default:
 			assert(0 && "Unhandled colour in getTexture");
         		return red_piece;
+	}
+}
+
+Texture2D get_dice(int value){
+	switch(value){
+		case 1: return dice1;
+		case 2: return dice2;
+		case 3: return dice3;
+		case 4: return dice4;
+		case 5: return dice5;
+		case 6: return dice6;
+		default:
+			assert(0 && "Invalid value in get_dice");
+			return dice1;
 	}
 }
 
